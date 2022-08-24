0650ce1adcb21767196c5259e414f344f7cad7d5
machine-56 <itwasanoption@gmail.com>
2022-08-24 16:32:14 +0530

    service file

---
+++ src/app/server.ts
@@ -0,0 +1,2 @@
+// export const serverURL = 'http://localhost:5465/api';
+export const serverURL = '/api';
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { serverURL } from '../server';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+
+  private logurl = `${serverURL}/login`;
 
   constructor() { }
 }
