0521caa12c12054240651a678129125b9113f905
Prajitha <98685133+prajithas@users.noreply.github.com>
2022-08-25 12:24:04 +0530

    dashboard link
    changed dashboard link, workorder form validation, service file creation

--- src/app/components/adminPage/admin-gen-workorder/admin-gen-workorder.component.ts
+++ src/app/components/adminPage/admin-gen-workorder/admin-gen-workorder.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-admin-gen-workorder',
   templateUrl: './admin-gen-workorder.component.html',
@@ -15,6 +14,8 @@ export class AdminGenWorkorderComponent implements OnInit {
   });
   trainingMode: any = ['Virtual', 'Physical', 'Hybrid'];
   selectedTaxType:string='';
+  currentDateTime:Date =new Date();
+  
   workOrder={
     pname:'',
     pid:'',
@@ -27,21 +28,23 @@ export class AdminGenWorkorderComponent implements OnInit {
     tax:'' ,
     panno:'',
     amount:'',
-    payterms:'' 
+    payterms:'',
+    issue_date:this.currentDateTime
   }; 
+ 
   constructor(private fb:FormBuilder) { }
   workOrderForm=this.fb.group({
     p_name:['', [Validators.required]],
     p_id:['', [Validators.required]],
     tp_name:['', [Validators.required]],
     t_topics:['', [Validators.required]],
-    start:[''],
-    end:[''],
+    start:['',[Validators.required]],
+    end:['',[Validators.required]],
     t_mode:['', [Validators.required]],
-    GST:[''],
+    GST:['',[Validators.minLength(15),Validators.maxLength(15),Validators.pattern("^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$")]],
     taxControl:['', [Validators.required]] ,
-    pan_no:['', [Validators.required]],
-    amount:['', [Validators.required]],
+    pan_no:['', [Validators.required,Validators.minLength(10),Validators.pattern("^[A-Z]{5}[0-9]{4}[A-Z]{1}$")]],
+    amount:['', [Validators.required,Validators.pattern("^[0-9]*$")]],
     pay_terms:['', [Validators.required]] 
   });
 
@@ -56,7 +59,7 @@ export class AdminGenWorkorderComponent implements OnInit {
   }
   onSubmit(){
     this.isSubmitted = true;
-    console.log(this.workOrderForm);
+    
     
   }
 
