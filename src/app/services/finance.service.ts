b547cb8b428f1479757f19ee13da4bfc0f81dc56
machine-56 <itwasanoption@gmail.com>
2022-08-29 13:35:10 +0530

    finance workorder

--- src/app/services/finance.service.ts
+++ src/app/services/finance.service.ts
@@ -18,9 +18,14 @@ export class FinanceService {
   eachWorkOrder(data: any){
     return this.http.post(`${this.logurl}/workorder/each`,{"woid":data});
   }
-
-  fnDeny(data: any){
+  
+  apvfn(data: any){
     console.log(data)
-    return this.http.delete(`${this.logurl}`+`/workorders/remove/`,data);
+    return this.http.put(`${this.logurl}/workorder/apv`,{"id":data})
   }
+
+  deletefn(data: any){
+    return this.http.delete(`${this.logurl}/workorder/remove/`+data)
+  }
+
 }
