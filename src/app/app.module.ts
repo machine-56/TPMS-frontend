ef7757b1590a84572bc1b104925e3ba897e09955
machine-56 <itwasanoption@gmail.com>
2022-09-05 12:38:22 +0530

    admin invoice change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker'; //date picker module
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -83,12 +83,12 @@ import { AdminApvPartnerComponent } from './components/adminPage/admin-apv-partn
     MatInputModule,
     MatNativeDateModule,
     MatRippleModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatIconModule,
     MatDividerModule,
     HttpClientModule,
+    // MatToolbarModule,
+    // MatSidenavModule,
+    // MatButtonModule,
+    // MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
